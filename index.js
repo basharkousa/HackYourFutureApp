f381580446481c7e54a046d2be3827cb310a5716
Bashar <basharkousax@gmail.com>
2026-06-29 21:02:27 +0200

    Feat: Finished Sprint2 (ReadLine, Number)

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log('Happy developing ✨')
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+function finish() {
+    rl.close();
+}
+
+function ask(question) {
+    return new Promise(function (resolve) {
+        rl.question(question, function (answer) {
+            resolve(answer);
+        });
+    });
+}
+
+function printMessage(val){
+    console.log(val);
+}
+
+async function main() {
+
+    //Introduction
+    printMessage('HackYourFuture Trainee Hub');
+    printMessage('Your go-to place for all HackYourFuture resources for trainees\n');
+
+    await ask("APPLY NOW - Press Enter to continue...");
+    const studyHours = Number(await ask("How many hours can you study per week? "))
+    printMessage(studyHours)
+    //We close the input when the app finished
+    finish();
+}
+
+main();
+
